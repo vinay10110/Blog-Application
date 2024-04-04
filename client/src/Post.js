681094d88135a73827e70043dc6fae849046e5f1
vinay <vinaychakravarthi10110@gmail.com>
2024-04-04 22:44:41 +0530

    script tag changed

--- client/src/Post.js
+++ client/src/Post.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 export default function Post({_id,title,summary,fileData,createdAt,author}) {
 
   return (
+    <div className="magnify" >
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
@@ -15,11 +16,12 @@ export default function Post({_id,title,summary,fileData,createdAt,author}) {
         <h2>{title}</h2>
         </Link>
         <p className="info">
-          <a href="#" className="author" >{author.username}</a>
+          <a href="/" className="author" >{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className="summary">{summary}</p>
       </div>
+    </div>
     </div>
   );
 }
