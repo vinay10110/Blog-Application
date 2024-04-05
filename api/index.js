1146330667011665d12bdd70201e3f5d6a5f2317
vinay <vinaychakravarthi10110@gmail.com>
2024-04-05 14:31:41 +0530

    changed the origin

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 const bodyParser=require('body-parser')
 app.use(cors({
-  origin: 'https://blog-application-lovat.vercel.app',
+  origin: 'https://blog-application-hhe1vd4bv-vinay10110s-projects.vercel.app',
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With'],
   credentials:true
