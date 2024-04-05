402eb4e0f2ab401b1db56eab557eb2a69d856a52
vinay <vinaychakravarthi10110@gmail.com>
2024-04-05 10:53:43 +0530

    cors added

--- api/index.js
+++ api/index.js
@@ -10,11 +10,13 @@ const cookieParser = require('cookie-parser');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 const bodyParser=require('body-parser')
+app.use(cors());
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'https://blogapp-rho-seven.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.sendStatus(200);
+  console.log("header.......");
 });
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
